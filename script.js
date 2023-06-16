17cc4a40069bc35ff64ace42dfb1dc1e635e7940
GOWTHAM M H <117714418+MHGOWTHAM@users.noreply.github.com>
2023-06-16 10:34:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here
 
+
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
+
+function sort(touristSpots) {
+	const regex=/^(a|an|the)\s/i
+	const compare=(a,b)=>{
+		const name1=a.replace(regex,'')
+		const name2=b.replace(regex,'')
+		return name1.localecompare(name2)
+	}
+	touristSpots.sort(compare)
+	return touristSpots
+}
+let array=sort(touristSpots)
+console.log(array)
+
